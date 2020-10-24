f0bee847f76bd27e59134caada134cee9a102a81
Corbin March <marchcorbin@gmail.com>
2020-10-24 14:56:01 -0600

    Made JobCard clickable to the id the user clicks on

--- src/Components/JobContainer.js
+++ src/Components/JobContainer.js
@@ -2,18 +2,21 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import './JobContainer.css'
 import JobCard from './JobCard'
+import { Link } from 'react-router-dom'
 
 import fakeData from '../FakeData'
 
 const JobContainer = () => {
-    const [allJobs, setAllJobs] = useState(fakeData)
+    const [allJobs, setAllJobs] = useState(fakeData.fakeJobs)
 
     const jobCards = allJobs.map(job => {
         return (
-            <JobCard 
-                {...job}
-                key={job.id}
-            />
+            <Link style={{textDecoration: 'none'}} to={`/details${job.id}`}>
+                <JobCard 
+                    {...job}
+                    key={job.id}
+                />
+            </Link>
         )
     })
     return (

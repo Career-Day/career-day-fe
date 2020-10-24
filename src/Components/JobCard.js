b39ff36ee2c5b734d286a6413be8f4c36e5607d4
Corbin March <marchcorbin@gmail.com>
2020-10-24 13:32:34 -0600

    Added styling to the job cards on the main page #14

--- src/Components/JobCard.js
+++ src/Components/JobCard.js
@@ -2,12 +2,16 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import './JobCard.css'
 
-const JobCard = ({title, imgURL, averageSalary, description}) => {
+const JobCard = ({title, imgURL, averageSalary, description, icon}) => {
     return (
-        <div>
+        <div className="individual-job"
+         >
+             <section className="title-and-icon">
+            <img style={{height:'4em'}} src={icon} alt='profession-icon' />
             <h3>{title}</h3>
             <p>{averageSalary}</p>
-            <p>{description}</p>
+             </section>
+            <p style={{margin: '.5em 0 1em .5em'}}>{description}</p>
         </div>
     )
 }

489ad261436a288c5773191c645fc18c5c01d853
Carly Clift <carlymclift@gmail.com>
2020-10-25 00:33:52 -0600

    Add icons to header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 import './Header.css'
 
 const Header = () => {
     return (
-        <div className="Header-section">
-            <Link className="back-button" to="/home-page" >Back</Link>
-            <h2>Career Day</h2>
-            <div>person</div>
-        </div>
+      <div className="header-section">
+        <Link className="back-button" to="/home-page" />
+        <h2 className="header-text" >Career Day</h2>
+        <button className="profile-image" ></button>
+      </div>
     )
 }
 

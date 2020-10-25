1ca10ea554a785d240142eaa1096675b7d31e782
Carly Clift <carlymclift@gmail.com>
2020-10-25 00:35:26 -0600

    Fix bug on landing page

--- src/PageViews/LandingPage.js
+++ src/PageViews/LandingPage.js
@@ -1,22 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 import './LandingPage.css'
 
 const LandingPage = () => {
-    
     return (
       <>
-        <h1>Welcome to Career Day!</h1>
         <div className="landing-page">
+          <h1>Welcome To<br />Career Day!</h1>
             <img 
+            alt="Cartoon of student on laptop"
             src="https://cdn.dribbble.com/users/15687/screenshots/11402762/media/b8eaf17dcdc71b1da589c3b737185da0.png" 
             className="landing-page-cartoon"
             />
-        <div className="landing-page-text-and-button">
-        <p>A place to find opportunities, search careers and get inspiration</p>
-        <Link to="/home-page" >Explore</Link>
-        </div>
+          <div className="landing-page-text-and-button">
+            <p>A place to find opportunities, search careers and get inspiration</p>
+            <Link to="/home-page" className="explore-button">Explore</Link>
+          </div>
         </div>
       </>
     )

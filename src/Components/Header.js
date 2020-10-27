d292024f0b5b19e3b422f00c67daec32f3d3c19b
Carly Clift <carlymclift@gmail.com>
2020-10-27 12:07:25 -0600

    Add additional styling

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,7 +6,8 @@ const Header = () => {
   return (
     <div className="header-section">
         <Link className="back-button" to="/home-page"></Link>
-        <h2>Career Day</h2>
+        <Link className="header-text" to="/">Career Day</Link>
+        {/* <h2>Career Day</h2> */}
         <button className="profile-image" ></button>
     </div>
   )
--- src/PageViews/JobDetails.js
+++ src/PageViews/JobDetails.js
@@ -1,34 +1,29 @@
 import React, { useEffect, useState } from 'react'
-import FakeData from '../FakeData'
 import './JobDetails.css'
 import Header from '../Components/Header'
-import colorData from '../ColorsData'
 import ReactPlayer from "react-player/youtube";
 import fetchSingleJob from '../Components/APICalls'
 
 const JobDetails = (props) => {
   const [currentDetails, setCurrentDetails] = useState([])
-  let random = Math.floor(Math.random() * (colorData.length))
 
   useEffect(() => {
     const getSingleJob = async() => {
-      console.log(props.match.params.id, 'iamtheid')
     const data = await fetchSingleJob(props.match.params.id)
-   setCurrentDetails(data.job)
+    setCurrentDetails(data.job)
   }
   getSingleJob()
-
   }, [])
 
   return (
-    <div className='job-details-page' style={{background: colorData[random].color}}>
+    <div className='job-details-page'>
     <Header />
       <div className="job-details-section">
         <img className="details-image" src={currentDetails.image_url} alt='Details' />
         <div className="details-text-holder">
-          <h2>{currentDetails.title}</h2>
-          <p><b>Salary Range: </b>${currentDetails.min_salary} -  ${currentDetails.max_salary}</p>
-          <p><b>Description:</b> {currentDetails.long_description}</p>
+          <h2 className="side-color">{currentDetails.title}</h2>
+          <p className="side-color"><b>Salary Range: </b>${currentDetails.min_salary} -  ${currentDetails.max_salary}</p>
+          <p className="side-color"><b>Description:</b> {currentDetails.long_description}</p>
         </div>
         <div className="player-wrapper">
           <ReactPlayer 

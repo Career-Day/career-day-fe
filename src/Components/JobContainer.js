0419730b21ff5a288647ac3f6ce7902b7d6d26eb
Carly Clift <carlymclift@gmail.com>
2020-10-27 12:09:24 -0600

    Add error handing messages #36

--- src/Components/JobContainer.js
+++ src/Components/JobContainer.js
@@ -3,23 +3,35 @@ import './JobContainer.css'
 import JobCard from './JobCard'
 import { Link } from 'react-router-dom'
 
-const JobContainer = ({displayedJobs}) => {
-  console.log(displayedJobs, 'displayedJobs')
-  
-  const jobCards = displayedJobs.map(job => {
+const JobContainer = ({displayedJobs, searching}) => {
+  if (displayedJobs.length === 0 && searching === true) {
     return (
-      <Link key={job.id} style={{textDecoration: 'none'}} to={`/details${job.id}`}>
-        <JobCard 
-          {...job}
-        />
-      </Link>
-    )
-  })
-  return (
       <div className="job-container">
-          {jobCards}
+        <p className="no-results-text">No careers that match that search<span role="img" aria-label="sad face emoji">☹️</span></p>
       </div>
-  )
+    )
+  } else if (displayedJobs.length === 0 && searching === false) {
+    return (
+      <div className="job-container">
+        <p className="no-results-text">Loading Jobs...</p>
+      </div>
+    )
+  } else {
+    const jobCards = displayedJobs.map(job => {
+      return (
+        <Link key={job.id} style={{textDecoration: 'none'}} to={`/details${job.id}`}>
+          <JobCard 
+            {...job}
+          />
+        </Link>
+      )
+    })
+    return (
+        <div className="job-container">
+          {jobCards}
+        </div>
+    )
+  }
 }
 
 export default JobContainer

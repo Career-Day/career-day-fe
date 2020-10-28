3579843edcf0b79e48a441461b0d331577e45399
Corbin March <marchcorbin@gmail.com>
2020-10-28 13:45:06 -0600

    Added passing test suite for LandingPage #40

---
+++ src/PageViews/LandingPage.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  render, screen
+} from '@testing-library/react';
+import LandingPage from './LandingPage'
+import {BrowserRouter} from 'react-router-dom'
+
+describe('LandingPage', () => {
+  it('should render a welcome message', () => {
+    render(<BrowserRouter><LandingPage /></BrowserRouter>)
+      let welcomeMessage = screen.getByText('Career Day!', {exact: false})
+      let welcomemsg = screen.getByText('A place to find opportunities, search careers and get inspiration')
+      expect(welcomeMessage).toBeInTheDocument()
+      expect(welcomemsg).toBeInTheDocument()
+  })
+
+  it('should render an image', () => {
+    render(<BrowserRouter><LandingPage /></BrowserRouter>)
+      let homeImage = screen.getByRole('img')
+      expect(homeImage).toBeInTheDocument()
+  })
+})

95f254d304ae3412afc0d611ab6f8cfc4cd0ef6b
Corbin March <marchcorbin@gmail.com>
2020-10-28 15:03:42 -0600

    Testing suite passing for the Header component #42

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,10 +5,10 @@ import './Header.css'
 const Header = () => {
   return (
     <div className="header-section">
-        <Link className="back-button" to="/home-page"></Link>
+        <Link alt='back-btn' className="back-button" to="/home-page"></Link>
         <Link className="header-text" to="/">Career Day</Link>
         {/* <h2>Career Day</h2> */}
-        <button className="profile-image" ></button>
+        <button alt='profile-image' className="profile-image" ></button>
     </div>
   )
 }

a6237664d9bfbb5b35411589ffbe297e56be65f9
Corbin March <marchcorbin@gmail.com>
2020-10-28 13:48:59 -0600

    merging with main branch

--- src/PageViews/LandingPage.test.js
+++ src/PageViews/LandingPage.test.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import {
-  render, screen
-} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import LandingPage from './LandingPage'
 import {BrowserRouter} from 'react-router-dom'
 
@@ -9,8 +7,10 @@ describe('LandingPage', () => {
   it('should render a welcome message', () => {
     render(<BrowserRouter><LandingPage /></BrowserRouter>)
       let welcomeMessage = screen.getByText('Career Day!', {exact: false})
+      let secondHalf = screen.getByText('Welcome To', {exact: false})
       let welcomemsg = screen.getByText('A place to find opportunities, search careers and get inspiration')
       expect(welcomeMessage).toBeInTheDocument()
+      expect(secondHalf).toBeInTheDocument()
       expect(welcomemsg).toBeInTheDocument()
   })
 

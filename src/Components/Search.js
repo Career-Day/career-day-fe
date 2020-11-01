737ed061704f4fb2dea2dace57d846d7b3d92aeb
Carly Clift <carlymclift@gmail.com>
2020-11-01 11:07:23 -0700

    Refactor code

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -5,22 +5,16 @@ import PropTypes from 'prop-types'
 
 const Search = ({ searchJobsByInput, searchJobsBySalaryRange }) => {
   const [advancedOpen, setAdvancedOpen] = useState(false)
-  const [searchInput, setSearchInput] = useState('')
   
   const hamToggle = () => {
     !advancedOpen ? setAdvancedOpen(true) : setAdvancedOpen(false)
-  }
-  
-  const changeHandler = (e) => {
-    setSearchInput(e.target.value)
-    searchJobsByInput(searchInput)
   }
   
   return (
     <>
       <div className="Search-section">
         <div className="search-box-button-sec">
-          <input onChange={changeHandler} type="text" placeholder="Search" className="search-box"/>
+          <input onChange={(e) => searchJobsByInput(e.target.value)} type="text" placeholder="Search" className="search-box"/>
           <button onClick={hamToggle} className="hamburger-menu"></button>
         </div>
       {advancedOpen && 

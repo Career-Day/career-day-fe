b0ce90351b3fcbc5479a48f1fa53941f57ead74a
Corbin March <marchcorbin@gmail.com>
2020-11-02 15:59:15 -0700

    Working functionality for the back button

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,39 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
 import App from './App'
 import { MemoryRouter } from 'react-router-dom'
 import '@testing-library/jest-dom'
+import {mockFetchAll, fakeData} from './FakeData'
+import {fetchAllJobs, fetchSingleJob} from './api/APICalls'
 import MutationObserver from '@sheerun/mutationobserver-shim'
-window.MutationObserver = MutationObserver
 jest.mock('./api/APICalls')
+window.MutationObserver = MutationObserver
 
 describe('App', () => {
 
   beforeEach( () => {
+    fetchAllJobs.mockResolvedValue([{
+            id: 1,
+            title: 'Graphic Designer',
+            icon_url: 'https://www.flaticon.com/premium-icon/icons/svg/3270/3270930.svg',
+            avg_salary: '$47k',
+            short_description: 'Graphic designers develop graphics and layouts for product illustrations, company logos, websites and more.'
+          },
+          {
+            id: 2,
+            title: 'Software Developer',
+            icon_url: 'https://www.flaticon.com/svg/static/icons/svg/2463/2463510.svg',
+            avg_salary: '$90k',
+            short_description: 'Researching, designing, implementing and managing software programs.'
+          },
+          {
+            id: 3,
+            title: 'Dentist',
+            icon_url: 'https://www.flaticon.com/svg/static/icons/svg/994/994888.svg',
+            avg_salary: '$170k',
+            short_description: 'Promote and assist in disease prevention and oral health.'
+          }] )
+  //   // fetchSingleJob.mockResolvedValue(mockFetchAll[0])
     render(
       <MemoryRouter>
         <App /> 
@@ -22,6 +46,18 @@ describe('App', () => {
     expect(screen.getByRole('img', { name: 'Cartoon of student on laptop'})).toBeInTheDocument()
     expect(screen.getByRole('link', { name: 'Explore' })).toBeInTheDocument()
     expect(screen.getByText('A place to find opportunities, search careers and get inspiration')).toBeInTheDocument()
+  })
+  it('Should be able to navigate to the home page see all of the jobs and then navigate to graphic designer', async() => {
+    // fetchAllJobs.mockResolvedValue(fakeData.fakeJobs)
+    expect(screen.getByRole('heading', { name: 'Welcome To Career Day!' })).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: 'Cartoon of student on laptop'})).toBeInTheDocument()
+    let explorebtn = (screen.getByRole('link', { name: 'Explore' }))
+    expect(explorebtn).toBeInTheDocument()
+    fireEvent.click(explorebtn)
+    // let eachJob = await waitFor( () => screen.getAllByRole('heading'))
+    let designer = await waitFor(() => screen.getByRole('heading', {name: 'Graphic Designer'}))
+    expect(designer).toBeInTheDocument()
+    // expect(eachJob.length).toBe(15)
   })
 })
 
--- src/PageViews/HomePage.test.js
+++ src/PageViews/HomePage.test.js
@@ -10,83 +10,45 @@ window.MutationObserver = MutationObserver;
 jest.mock('../api/APICalls')
 
 describe('HomePage', () => {
-
-  it('should render a search input bar', () => {
-    render (
+  beforeEach(() => {
+    fetchAllJobs.mockResolvedValue(mockFetchAll);
+    render(
       <MemoryRouter>
         <HomePage />
       </MemoryRouter>
-    )
+    );
+  })
 
+  it('should render a search input bar', () => {
     const searchInput = screen.getByPlaceholderText('Search')
     expect(searchInput).toBeInTheDocument()
   })
 
   it('should render a loading message', () => {
-    render (
-      <MemoryRouter>
-        <HomePage />
-      </MemoryRouter>
-    )
-
     let loadingMessage = screen.getByText('Loading Jobs...')
     expect(loadingMessage).toBeInTheDocument()
   })
 
   it('should render all jobs', async() => {
-   fetchAllJobs.mockResolvedValue(mockFetchAll)
-
-    render (
-      <MemoryRouter>
-        <HomePage />
-      </MemoryRouter>
-    )
-
     let singleJob = await waitFor(() => screen.getAllByRole('heading'))
     expect(singleJob.length).toBe(2)
+    expect(fetchAllJobs).toHaveBeenCalled()
   })
 
   it('should allow a user to search for a job', async () => {
-    fetchAllJobs.mockResolvedValue(mockFetchAll)
-
-    render  (
-      <MemoryRouter>
-        <HomePage />
-      </MemoryRouter>
-    )
-
     let searchInput = screen.getByPlaceholderText("Search");
     expect(searchInput).toBeInTheDocument()
     await waitFor(() => fireEvent.change(searchInput, {target: {name: 'search', value: 'Graphic'}}))
     expect(searchInput.value).toBe('Graphic')
+
     // Cant get results to populate
   })
 
   it('should have a hamburger button that opens access to a slider', async() => {
-    render (
-      <MemoryRouter>
-        <HomePage />
-      </MemoryRouter>
-    )
-
     let burgerbtn = screen.getByTestId('burgerbtn')
     expect(burgerbtn).toBeInTheDocument()
     fireEvent.click(burgerbtn)
     let slider = await waitFor(() => screen.getByText('Salary Range:'))
     expect(slider).toBeInTheDocument()
   })
-
-  // it('should have a description for each job', async() => {
-  //   fetchAllJobs.mockResolvedValue(mockFetchAll)
-
-  //   render (
-  //     <MemoryRouter>
-  //       <HomePage />
-  //     </MemoryRouter>
-  //   )
-
-  //   let singleDescription = await waitFor(() =>screen.getByTestId('description'))
-  //   expect(fetchAllJobs).toHaveBeenCalled();
-  //   expect(singleDescription).toBeInTheDocument()
-  // })
 })

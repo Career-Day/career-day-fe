d9496b2c58e4fd4a1575f3ce88bf88d9eed70803
Carly Clift <carlymclift@gmail.com>
2020-11-01 20:36:55 -0700

    Update service worker fetch

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -9,23 +9,48 @@ self.addEventListener('install', (event) => {
     caches.open(CACHE_NAME)
       .then((cache) => {
         console.log('Opened cache')
-        
         return cache.addAll(urlsToCache)
      })
   )
 })
 
-// listen for requests
+// Listen for requests
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(() => {
-        return fetch(event.request)
-          .catch(() => caches.match('offline.html'))
+    caches.match(event.request).then(cacheRes => {
+      return cacheRes || fetch(event.request).then(fetchRes => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request.url, fetchRes.clone())
+            return fetchRes
+        })
       })
+    }).catch(() => {
+      if(event.request.url.indexOf('.html') > -1){
+        return caches.match('offline.html')
+      }
+    })
   )
 })
+
+// self.addEventListener('fetch', function (event) {
+//   event.respondWith(
+//     // Try the cache
+//     caches
+//       .match(event.request)
+//       .then(function (response) {
+//         // Fall back to network
+//         return response || fetch(event.request);
+//       })
+//       .catch(function () {
+//         // If both fail, show a generic fallback:
+//         return caches.match('/offline.html');
+//         // However, in reality you'd have many different
+//         // fallbacks, depending on URL and headers.
+//         // Eg, a fallback silhouette image for avatars.
+//       }),
+//   );
+// });
 
 // Activate SW
 

d5a45913d47a2d5399442f0aa02203d6d0144d7e
Corbin March <marchcorbin@gmail.com>
2020-11-04 10:25:53 -0700

    Added data logic to the homepage test suite

--- src/PageViews/HomePage.test.js
+++ src/PageViews/HomePage.test.js
@@ -26,14 +26,20 @@ fetchAllJobs.mockResolvedValue(mockFetchAll)
   it('should allow a user to search for a job', async () => {
     let searchInput = screen.getByPlaceholderText("Search");
     expect(searchInput).toBeInTheDocument()
+    let eachJob = await waitFor(() => screen.getAllByTestId('singular-job'))
+    expect(eachJob.length).toBe(15)
     await waitFor(() => fireEvent.change(searchInput, {target: {name: 'search', value: 'Graphic'}}))
     expect(searchInput.value).toBe('Graphic')
   })
 
-  it('should have a hamburger button that opens access to a slider', async() => {
+  it('should have a hamburger button that closes access to a slider', async() => {
     let slider = await waitFor(() => screen.getByText('Salary Range:'))
     let burgerbtn = screen.getByTestId('burgerbtn')
     expect(slider).toBeInTheDocument()
     expect(burgerbtn).toBeInTheDocument()
+  })
+  it('should render all of the given jobs', async() => {
+    let eachJob = await waitFor(() => screen.getAllByTestId('singular-job'))
+    expect(eachJob.length).toBe(15)
   })
 })
